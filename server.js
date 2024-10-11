74b57f55e15b2e14ffe0c02d0b56285373217aa6
jheferlinares <jhefer98@hotmail.com>
2024-10-11 02:46:04 -0400

    por sia

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 
 // View Engine and Templates
@@ -26,6 +27,8 @@ app.use(static)
 //Index route
 app.get("/", baseController.buildHome)
 
+// Inventory routes
+app.use("/inv", inventoryRoute)
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
